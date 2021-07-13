8eb3c67abf5ea1fd692518fc8c7d58808e9b666b
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2021-07-13 17:58:40 +0200

    feat(project): :hankey: add IUsersService and UserService

---
+++ AWSServerlessApplication/Services/Interfaces/IUsersService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSServerlessApplication.Services.Interfaces
+{
+    public interface IUsersService
+    {
+    }
+}
---
+++ AWSServerlessApplication/Services/UsersService.cs
@@ -0,0 +1,15 @@
+﻿using AWSServerlessApplication.Models;
+using AWSServerlessApplication.Services.Interfaces;
+
+using System.Threading.Tasks;
+
+namespace AWSServerlessApplication.Services
+{
+    public class UsersService : IUsersService
+    {
+        public Task<User> GetAsync(string name)
+        {
+            return null;
+        }
+    }
+}
