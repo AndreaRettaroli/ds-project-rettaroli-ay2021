b8c2acf1faefdb4704952966b0167d37124ca3c9
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2021-07-13 18:13:16 +0200

    feat(project): :construction: add user table and password utils

---
+++ AWSServerlessApplication/Utils/PasswordUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSServerlessApplication.Utils
+{
+    public class PasswordUtils
+    {
+        internal static string Generate(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
