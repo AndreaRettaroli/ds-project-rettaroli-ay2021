178015c53519fd98e94542c81bbbb7a1d504b9a4
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2021-07-13 18:16:06 +0200

    feat(project): :construction: user request, done forgot password request

---
+++ AWSServerlessApplication/Models/ForgotPasswordRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSServerlessApplication.Models
+{
+    public class ForgotPasswordRequest
+    {
+        public string UserId { get; set; }
+        public string VerificationCode { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
