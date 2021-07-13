cde20acfed2187308758988c39b0257415dc7fbe
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2021-07-13 18:07:34 +0200

    feat(project): :zap: add auth service

---
+++ AWSServerlessApplication/Services/AuthService.cs
@@ -0,0 +1,20 @@
+
+using AWSServerlessApplication.Models;
+using AWSServerlessApplication.Services.Interfaces;
+
+namespace AWSServerlessApplication.Services
+{
+    public class AuthService : IAuthService
+    {
+        public DynamoDBUser AuthUser { get; private set; }
+
+        public AuthService()
+        {
+        }
+
+        public void Init(DynamoDBUser user)
+        {
+            AuthUser = user;
+        }
+    }
+}
