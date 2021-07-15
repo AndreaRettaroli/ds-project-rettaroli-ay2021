398198780df60880ea6770f7dbc922acba25ee72
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2021-07-15 22:03:31 +0200

    feat(project): :construction: prepare to send reset password mail by template on s3

---
+++ AWSServerlessApplication/AWS/IS3Service.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSServerlessApplication.AWS
+{
+    interface IS3Service
+    {
+        Task<string> GetHtmlFileAsync(string objectKey);
+        Task<Stream> GetObjectStream(string objectKey);
+    }
+}
