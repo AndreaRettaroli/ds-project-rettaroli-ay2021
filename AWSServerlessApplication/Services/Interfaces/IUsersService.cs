02d518d8e3a754e279b77d819bed09b5547041bd
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2021-07-15 21:56:47 +0200

    feat(project): :art: define userService interface

--- AWSServerlessApplication/Services/Interfaces/IUsersService.cs
+++ AWSServerlessApplication/Services/Interfaces/IUsersService.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using AWSServerlessApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AWSServerlessApplication.Authentication;
 
 namespace AWSServerlessApplication.Services.Interfaces
 {
     public interface IUsersService
     {
+        Task<User> GetAsync(string id);
+        Task<IEnumerable<User>> ListAsync();
+        Task<User> CreateAsync(DynamoDBUser userRequest);
+        Task<User> UpdateAsync(DynamoDBUser userRequest);
+        Task<User> SetPasswordAsync(Credentials credentials);
+        Task<bool> DeleteAsync(string id);
     }
 }
