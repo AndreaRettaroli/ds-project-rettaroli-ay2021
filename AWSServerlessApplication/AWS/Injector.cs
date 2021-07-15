d1d1ae7c5723804951dbf382308f1053bb0cad0f
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2021-07-15 22:01:53 +0200

    feat(project): :ok_hand: add lambda injector for s3

---
+++ AWSServerlessApplication/AWS/Injector.cs
@@ -0,0 +1,27 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.S3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSServerlessApplication.AWS
+{
+    public class Injector
+    {
+        private static string regionCode;
+        private static RegionEndpoint region;
+        public Injector()
+        {
+            regionCode = Environment.GetEnvironmentVariable("AWS_DEFAULT_REGION") ?? "eu-west-1";
+            region = Amazon.RegionEndpoint.GetBySystemName(regionCode);
+
+        }
+        public IAmazonS3 CreateS3()
+        {
+            return new AmazonS3Client(region);
+        }
+
+    }
+}
