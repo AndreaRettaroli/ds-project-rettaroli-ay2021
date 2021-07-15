7c983259692484ea9b4a9518b2bf6d2cb81d9c9a
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2021-07-15 21:59:16 +0200

    feat(project): :ok_hand: complete service and dynamo request

---
+++ AWSServerlessApplication/Services/DynamoDBRequests/UserRequest.cs
@@ -0,0 +1,60 @@
+﻿using Amazon.DynamoDBv2.Model;
+using AWSServerlessApplication.Extensions;
+using AWSServerlessApplication.Models;
+using AWSServerlessApplication.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSServerlessApplication.Services.DynamoDBRequests
+{
+    public static class UserRequest
+    {
+        private static readonly string _tableName = UsersTable.TableName;
+
+        public static PutItemRequest Insert(DynamoDBUser user)
+        {
+            return new PutItemRequest
+            {
+                TableName = _tableName,
+                Item = user.ToAttributeMap()
+            };
+        }
+        public static UpdateItemRequest Confirm(string id, DateTime now)
+        {
+            return new UpdateItemRequest
+            {
+                TableName = _tableName,
+                Key = id.ToDynamoDBKey(UsersTable.Id),
+                UpdateExpression = "SET #confirmedAt = :v_confirmedAt",
+                ExpressionAttributeNames = new Dictionary<string, string>
+                {
+                    {"#confirmedAt", UsersTable.ConfirmedAt}
+                },
+                ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+                {
+                    {":v_confirmedAt", new AttributeValue(now.ToString(Constants.DateTimeFormat)) }
+                }
+            };
+        }
+        public static UpdateItemRequest DeleteAsync(string id) => new UpdateItemRequest
+        {
+            TableName = _tableName,
+            Key = DynamoDBUtils.GenerateKey(UsersTable.Id, id),
+            UpdateExpression = "SET #d = :v_del",
+            ConditionExpression = "attribute_exists(#i)",
+            ExpressionAttributeNames = new Dictionary<string, string>
+            {
+                { "#i", UsersTable.Id },
+                { "#d", UsersTable.Deleted }
+            },
+            ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+            {
+                { ":v_del", new AttributeValue(DateTimeExtensions.CestNow.ToString(Constants.DateTimeFormat)) }
+            },
+            ReturnValues = "UPDATED_NEW"
+        };
+
+    }
+}
--- AWSServerlessApplication/Services/UsersService.cs
+++ AWSServerlessApplication/Services/UsersService.cs
@@ -1,15 +1,115 @@
-﻿using AWSServerlessApplication.Models;
+﻿using Amazon.DynamoDBv2;
+using AWSServerlessApplication.Authentication;
+using AWSServerlessApplication.AWS;
+using AWSServerlessApplication.Extensions;
+using AWSServerlessApplication.Models;
+using AWSServerlessApplication.Services.DynamoDBRequests;
 using AWSServerlessApplication.Services.Interfaces;
-
+using AWSServerlessApplication.Utils;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace AWSServerlessApplication.Services
 {
     public class UsersService : IUsersService
     {
-        public Task<User> GetAsync(string name)
+        private readonly IAuthService _authService;
+        private readonly ICognitoService _cognito;
+        private readonly IAmazonDynamoDB _dynamoDB;
+        private readonly string _tableName;
+        public UsersService(IAuthService authService,
+            IAmazonDynamoDB dynamoDB,
+            ICognitoService cognito)
         {
-            return null;
+            _authService = authService;
+            _dynamoDB = dynamoDB;
+            _cognito = cognito;
+            _tableName = UsersTable.TableName;
+
+        }
+        public async Task<User> CreateAsync(DynamoDBUser user)
+        {
+            try
+            {
+                var response = await _cognito.CreateAsync(user.Email);
+                user.Id = response.User.Username;
+                var request = UserRequest.Insert(user);
+                await _dynamoDB.PutItemAsync(request);
+                return request.Item.ToDynamoDBUser().ToUser();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(string id)
+        {
+            try
+            {
+                var request = UserRequest.DeleteAsync(id);
+                var response = await _dynamoDB.UpdateItemAsync(request);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<User> GetAsync(string id)
+        {
+            try
+            {
+                var result = await _dynamoDB.GetItemAsync(_tableName, id.ToDynamoDBKey(UsersTable.Id));
+                return result.Item.ToDynamoDBUser().ToUser();
+            }
+            catch
+            {
+                return null; 
+            }
+        }
+
+        public Task<IEnumerable<User>> ListAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<User> SetPasswordAsync(Credentials credentials)
+        {
+            try
+            {
+                var now = DateTime.UtcNow;
+                var user = _authService.AuthUser;
+                var response = await _cognito.SetUserPasswordAsync(user.Email, credentials.Password);
+                var request = UserRequest.Confirm(user.Id, now);
+                await _dynamoDB.UpdateItemAsync(request);
+                user.ConfirmedAt = now.TruncateToSecondStart();
+                return user.ToUser();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<User> UpdateAsync(DynamoDBUser user)
+        {
+            try
+            {
+                var currentUser = await GetAsync(user.Id);
+                if (currentUser == null || currentUser.Deleted != null)
+                    return null;
+
+                var request = UserRequest.Insert(user);
+                await _dynamoDB.PutItemAsync(request);
+                return request.Item.ToDynamoDBUser().ToUser();
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
