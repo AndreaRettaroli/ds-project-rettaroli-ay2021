124fe5e228a3e40577b3f3f225ee699ec89f9034
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2021-07-15 21:38:27 +0200

    feat(project): :pizza: add static utils function for types

---
+++ AWSServerlessApplication/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSServerlessApplication.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime TruncateToSecondStart(this DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second);
+        }
+        public static DateTime Cest(this DateTime dateTime)
+        {
+            TimeZoneInfo romeTimeZoneInfo;
+            try
+            {
+                romeTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
+            }
+            catch
+            {
+                romeTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Europe/Rome");
+            }
+            return TimeZoneInfo.ConvertTime(dateTime, romeTimeZoneInfo);
+        }
+        public static DateTime CestNow => DateTime.UtcNow.Cest();
+    }
+}
