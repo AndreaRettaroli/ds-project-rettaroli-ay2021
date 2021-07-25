7cb5418476c56fe2286b56c49114b410b5ad2b60
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2021-07-25 16:36:49 +0200

    feat(project): :zap: split create and modify request

--- AWSServerlessApplication/Models/Request/ModifyUserRequest.cs
+++ AWSServerlessApplication/Models/Request/ModifyUserRequest.cs
@@ -5,10 +5,10 @@ using System.Threading.Tasks;
 
 namespace AWSServerlessApplication.Models
 {
-    public class CreateModifyUserRequest
+    public class ModifyUserRequest
     {
         public string Id { get; set; }
-        public string Email { get; set; }
+        public string ImageUrl { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
     }
