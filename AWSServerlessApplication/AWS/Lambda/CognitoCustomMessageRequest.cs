119cd1268d2894845f9199366240cb5276a60b51
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2021-07-25 16:35:29 +0200

    feat(project): :zap: add handler, change namespace, add LambdaSerializer

--- AWSServerlessApplication/AWS/Lambda/CognitoCustomMessageRequest.cs
+++ AWSServerlessApplication/AWS/Lambda/CognitoCustomMessageRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace AWSServerlessApplication.AWS.Lambda
+namespace AWS.Lambda
 {
     public class CognitoCustomMessageRequest
     {
--- AWSServerlessApplication/AWS/Lambda/CognitoCustomMessagesFunction.cs
+++ AWSServerlessApplication/AWS/Lambda/CognitoCustomMessagesFunction.cs
@@ -1,14 +1,20 @@
 ﻿using Amazon.Lambda.Core;
+using AWSServerlessApplication.AWS;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AWSServerlessApplication.AWS.Lambda
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AWS.Lambda
 {
     public class CognitoCustomMessagesFunction
     {
+        private const string FORGOT_PASSWORD_TRIGGER = "CustomMessage_ForgotPassword";
+        private const string REGISTER_USER_TRIGGER = "CustomMessage_AdminCreateUser";
+        private const string FIRST_PSW_HTML_BUCKET_KEY = "first_password.html";
         private const string USERNAME_PLACEHOLDER = "{##USERNAME##}";
         private const string WEBSITE_URL_PLACEHOLDER = "{##URL##}";
         private const string FORGOT_PSW_HTML_BUCKET_KEY = "forgot_password.html";
@@ -27,7 +33,6 @@ namespace AWSServerlessApplication.AWS.Lambda
                 var s3Client = injector.CreateS3();
                 _s3Service = new S3Service(s3Client, bucketName);
                 var triggerSource = cognitoEvent.TriggerSource;
-
                 context?.Logger.LogLine(triggerSource);
                 var result = HandleTriggerAsync(triggerSource).GetAwaiter().GetResult();
                 context?.Logger.LogLine(JsonConvert.SerializeObject(result));
@@ -45,11 +50,19 @@ namespace AWSServerlessApplication.AWS.Lambda
         {
             string subject = string.Empty;
             string body = string.Empty;
-            subject = "Reset your password";
-            body = await _s3Service.GetHtmlFileAsync(FORGOT_PSW_HTML_BUCKET_KEY);
-            var username = _cognitoEvent.UserName.ToString();
-            body = body.Replace(USERNAME_PLACEHOLDER, username);
-            body = body.Replace(WEBSITE_URL_PLACEHOLDER, Environment.GetEnvironmentVariable("WebsiteUrl") ?? "https://localhost:44357");//TODO:sistemare post pubblicazione
+            switch (triggerSource) {
+                case FORGOT_PASSWORD_TRIGGER:
+                    subject = "Reset your password";
+                    body = await _s3Service.GetHtmlFileAsync(FORGOT_PSW_HTML_BUCKET_KEY);
+                    var username = _cognitoEvent.UserName.ToString();
+                    body = body.Replace(USERNAME_PLACEHOLDER, username);
+                    break;
+                case REGISTER_USER_TRIGGER:
+                    subject = "Benvenuto!";
+                    body = await _s3Service.GetHtmlFileAsync(FIRST_PSW_HTML_BUCKET_KEY);
+                    break;
+            }; 
+            body = body.Replace(WEBSITE_URL_PLACEHOLDER, "https://7ge1zdatk1.execute-api.eu-west-1.amazonaws.com/Prod/swagger/index.html" );
             CustomizeResponse(subject, body);
             return _cognitoEvent;
 
